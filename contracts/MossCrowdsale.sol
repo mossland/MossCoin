55d63963490c8ff3cf0301a676f21ce335ac04ee
namhyunuk <jwvg0425@naver.com>
2018-01-29 10:38:40 +0900

    capEther -> cap

--- contracts/MossCrowdsale.sol
+++ contracts/MossCrowdsale.sol
@@ -9,8 +9,8 @@ contract MossCrowdsale is CappedCrowdsale {
     uint256[5] public values; // bonus by send ether amount criteria
     uint256[4] public ends; // bonus by ico timing criteria
 
-    function MossCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _capEther, uint256 _minInvestFinney, uint256 _maxInvestFinney, address _wallet, address _token) public
-        CappedCrowdsale(_capEther)
+    function MossCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, uint256 _minInvestFinney, uint256 _maxInvestFinney, address _wallet, address _token) public
+        CappedCrowdsale(_cap)
         Crowdsale(_startTime, _endTime, _rate, _minInvestFinney, _maxInvestFinney, _wallet, _token) 
     {
         values = [5 * 1 ether, 10 * 1 ether, 25 * 1 ether, 75 * 1 ether, _maxInvestFinney * 1 finney + 1];
@@ -39,8 +39,8 @@ contract MossCrowdsale is CappedCrowdsale {
 
 //main ico crowdsale contract
 contract MossCrowdsalePre is MossCrowdsale {
-    function MossCrowdsalePre(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _capEther, uint256 _minInvestFinney, uint256 _maxInvestFinney, address _wallet, address _token) public
-        MossCrowdsale(_startTime, _endTime, _rate, _capEther, _minInvestFinney, _maxInvestFinney, _wallet, _token)
+    function MossCrowdsalePre(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, uint256 _minInvestFinney, uint256 _maxInvestFinney, address _wallet, address _token) public
+        MossCrowdsale(_startTime, _endTime, _rate, _cap, _minInvestFinney, _maxInvestFinney, _wallet, _token)
     {
         ends = [_endTime + 1, _endTime + 1, _endTime + 1, _endTime + 1];
         bonus[0] = [1350, 1400, 1450, 1500, 1550];
